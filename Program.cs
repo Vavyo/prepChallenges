3c17ca8534d0bd772bf42b53a144724201e617a1
Vavyo <vavyo2121@gmail.com>
2021-03-21 23:18:16 -0500

    challenge 1 done

--- Program.cs
+++ Program.cs
@@ -6,7 +6,27 @@ namespace prepChallenges
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] inputArr = new int[5];
+            Console.WriteLine("Enter 5 numbers, 1-10, separately:");
+            for (int i = 0; i < inputArr.Length; i++)
+            {
+                inputArr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("Enter a number:");
+            int compareNum = Convert.ToInt32(Console.ReadLine());
+            arrayMaxResult(inputArr, compareNum);
+        }
+        static void arrayMaxResult(int[] arr, int num)
+        {
+            int score = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == num)
+                {
+                    score += num;
+                }
+            }
+            Console.WriteLine("You Scored:" + score);
         }
     }
 }
